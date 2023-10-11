c781187522d0615b147667ad19926bbfdab94418
Marc Marcellus <marc.marcellus@gmail.com>
2023-10-11 20:03:56 +0000

    #31 we enable S3 Static Website Hosting

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,6 @@
 output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
+output "website_enpoint" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
