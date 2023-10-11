7d8a69de5f3195d90b4225edf6520279e84f877b
marcalfa1 <marc.marcellus@gmail.com>
2023-10-11 16:16:27 -0400

    #31 we enable S3 Static Website Hosting (#36)

--- variables.tf
+++ variables.tf
@@ -4,4 +4,15 @@ variable "user_uuid" {
 
 variable "bucket_name" {
  type = string
+<<<<<<< HEAD
+}
+
+variable "index_html_filepath" {
+  type = string
+}
+
+variable "error_html_filepath" {
+  type = string
+=======
+>>>>>>> main
 }
