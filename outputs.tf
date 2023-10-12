f63da21ccdb9f6b7fe8a0b79e70b165c7cba95d7
Marc Marcellus <marc.marcellus@gmail.com>
2023-10-12 01:35:00 +0000

    #41 invalidate cloudfront starck when content version changes

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_website_enpoint" {
   description = "S3 Static Website hosting endpoint"
   value = module.terrahouse_aws.website_enpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
