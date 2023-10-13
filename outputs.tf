a09ddc9fbe877ca2b4bac7964ddacb4999d8653b
marcalfa1 <marc.marcellus@gmail.com>
2023-10-13 16:53:25 -0400

    49 terratowns provider (#51)
    * #49 setting up skeleton for creating tf custom provider
    
    * #49 implement tf provider block in tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_enpoint" {
-  description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_enpoint
-}
+# output "s3_website_enpoint" {
+#   description = "S3 Static Website hosting endpoint"
+#   value = module.terrahouse_aws.website_enpoint
+# }
 
-output "cloudfront_url" {
-  description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The CloudFront Distribution Domain Name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
