c8f6c49ce56f98de43b418fa3e56047b0f9b6985
marcalfa1 <marc.marcellus@gmail.com>
2023-10-14 01:14:16 -0400

    #56 fix issues with provider, setup two towns (#57)

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  #cloud {
-  #  organization = "ExamPro"
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
+  cloud {
+    organization = "marcalfa1"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 
 }
 
@@ -28,15 +28,12 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_e30m3_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.e30m3.public_path
+  content_version = var.e30m3.content_version
 }
-
 
 resource "terratowns_home" "home" {
   name = "BMW E30 M3"
@@ -46,7 +43,27 @@ produced by BMW's in-house motorsport division,
 BMW M. Manufactured between 1986 and 1991, 
 the E30 M3 is celebrated as one of the finest drivers' cars ever made and has a rich motorsport heritage.
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_e30m3_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.e30m3.content_version
+}
+
+module "home_e36m3_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.e36m3.public_path
+  content_version = var.e36m3.content_version
+}
+
+resource "terratowns_home" "home_e36m3" {
+ name = "BMW E36 M3"
+  description = <<DESCRIPTION
+BMW E30 M3 is an iconic high-performance version of the BMW 3 Series, 
+produced by BMW's in-house motorsport division, 
+BMW M. Manufactured between 1986 and 1991, 
+the E30 M3 is celebrated as one of the finest drivers' cars ever made and has a rich motorsport heritage.
+DESCRIPTION
+  domain_name = module.home_e36m3_hosting.domain_name
+  town = "missingo"
+  content_version = var.e36m3.content_version
 }
