6128f429a811f93a3366f2e030226b5557252ff7
Marc Marcellus <marc.marcellus@gmail.com>
2023-10-14 05:12:40 +0000

    #56 fix issues with provider, setup two towns

--- modules/terrahome_aws/ouputs.tf
+++ modules/terrahome_aws/ouputs.tf
@@ -2,10 +2,10 @@ output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
 
-output "website_enpoint" {
+output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
 
-output "cloudfront_url" {
+output "domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
